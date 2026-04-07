dbb44c4daa2fe2f2143c8917a380c35a852aa43b
Sundar M. Bimali <Sundar.Bimali@Dream-iMac.local>
2026-04-07 15:39:15 +0545

    set domain to onlipane

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({ site: 'https://232e89r837ryufghe.github.io', base: '/online-pati-news' });
+export default defineConfig({ site: 'https://onlinepatinews.com' });
